fe4645053b020406e5732bc6d4b0fbbc617d4239
Senis <thank243@gmail.com>
2024-02-02 19:03:07 +0800

    Implement retry mechanism for fetchRss function
    The commit introduces a retry mechanism for the function 'fetchRss', which attempts to fetch data up to 3 times before returning an error. This is useful because network calls, like the one to fetch RSS data, can be inconsistent or unreliable. The error message has also been updated to provide more details about the unsuccessful attempts.

--- app.go
+++ app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,14 +14,27 @@ func handler(c echo.Context) error {
 		return c.String(http.StatusForbidden, "token is null")
 	}
 
-	rssData, err := fetchRss(token)
+	var (
+		rssData []byte
+		err     error
+	)
+
+	for i := 0; i < 3; i++ {
+		rssData, err = fetchRss(token)
+		if err == nil {
+			break
+		}
+
+		time.Sleep(time.Second * 3)
+	}
 	if err != nil {
-		return c.String(http.StatusServiceUnavailable, err.Error())
+		return c.String(http.StatusServiceUnavailable,
+			fmt.Sprintf("Failed to fetch RSS content after retrying 3 times. The last err: %v", err))
 	}
 
 	data, err := fixDate(rssData)
 	if err != nil {
-		return c.String(http.StatusForbidden, err.Error())
+		return c.String(http.StatusServiceUnavailable, err.Error())
 	}
 
 	return c.Blob(http.StatusOK, echo.MIMEApplicationXMLCharsetUTF8, data)
